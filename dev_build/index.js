09433db5e7027d09906f48807c44ba9755155749
Pelin <pelinakdulun2020@gmail.com>
2025-09-30 11:22:34 +0300

    routing

---
+++ dev_build/index.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hey!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxNQUFNLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL0hzZUdpdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGV5IScpOyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
