fade1a69774b060529f91dc0c58a3a0587074f14
Pelin <pelinakdulun2020@gmail.com>
2025-10-07 11:29:21 +0300

    Improve code formatting and update build artifacts
    - Format webpack.pages.js with consistent indentation and semicolons
    - Update build outputs in docs/ and dev_build/ directories
    - Maintain proper JavaScript code style standards

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -1,16 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-function createPages(template, filename){
-    return new HtmlWebpackPlugin({
-        template: template,
-        filename: filename
-      })
+function createPages(template, filename) {
+  return new HtmlWebpackPlugin({
+    template: template,
+    filename: filename,
+  });
 }
-const htmlPages=[
-    createPages('./src/index.html', './index.html'),
-    createPages('./src/pages/mainpage/mainpage.html', './pages/mainpage/mainpage.html'),
-    createPages('./src/pages/magazine/magazine.html', './pages/magazine/magazine.html'),
-    createPages('./src/pages/podcasts/podcasts.html','./pages/podcasts/podcasts.html'),
-    createPages('./src/pages/folders/folders.html','./pages/folders/folders.html'),
-    createPages('./src/pages/tests/test1.html','./pages/tests/test1.html')
-]
-module.exports=htmlPages
+const htmlPages = [
+  createPages("./src/index.html", "./index.html"),
+  createPages(
+    "./src/pages/mainpage/mainpage.html",
+    "./pages/mainpage/mainpage.html"
+  ),
+  createPages(
+    "./src/pages/magazine/magazine.html",
+    "./pages/magazine/magazine.html"
+  ),
+  createPages(
+    "./src/pages/podcasts/podcasts.html",
+    "./pages/podcasts/podcasts.html"
+  ),
+  createPages(
+    "./src/pages/folders/folders.html",
+    "./pages/folders/folders.html"
+  ),
+  createPages("./src/pages/tests/test1.html", "./pages/tests/test1.html"),
+];
+module.exports = htmlPages;
