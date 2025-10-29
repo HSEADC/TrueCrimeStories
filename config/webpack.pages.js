620591dee664febe4ebaabd4b0fb179e3c22d478
Maschenko Sofia <m.sofia05@mail.ru>
2025-10-29 17:20:44 +0300

    delete promo.html, add stylegaid.html

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -9,13 +9,12 @@ function createPages(template, filename) {
 
 const htmlPages = [
   createPages("./src/index.html", "./index.html"),
-  createPages("./src/promo.html", "./promo.html"),
   createPages("./src/about.html", "./about.html"),
   createPages("./src/newspaper.html", "./newspaper.html"),
   createPages("./src/tests.html", "./tests.html"),
   createPages("./src/files.html", "./files.html"),
   createPages("./src/podcasts.html", "./podcasts.html"),
-
+  createPages("./src/stylegaid.html", "./stylegaid.html"),
 ];
 
 module.exports = htmlPages;
